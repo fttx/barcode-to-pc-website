65628d1b414b8f712e6d512b8481fd2fc7c55456
filippo <filippo.tortomasi@gmail.com>
2017-10-02 12:50:17 +0200

    Updated libraries Fix javascript error

--- js/new-age.js
+++ js/new-age.js
@@ -2,15 +2,15 @@
     "use strict"; // Start of use strict
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function (event) {
-        var target = $($(this).attr('href'));
-        if (target.length) {
-            $('html, body').stop().animate({
-                scrollTop: (target.offset().top - 50)
-            }, 1250, 'easeInOutExpo');
-            event.preventDefault();
-        }
-    });
+    // $('a.page-scroll').bind('click', function (event) {
+    //     var target = $($(this).attr('href'));
+    //     if (target.length) {
+    //         $('html, body').stop().animate({
+    //             scrollTop: (target.offset().top - 50)
+    //         }, 1250, 'easeInOutExpo');
+    //         event.preventDefault();
+    //     }
+    // });
 
     // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
